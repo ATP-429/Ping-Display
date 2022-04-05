7712dd67581ba160d448859e7144420da94cfbf8
ATP-429 <atpgaming123@gmail.com>
2022-04-05 22:42:39 +0530

    Make dragging more responsive by having only one start and end drag points instead of small incremental drags

--- app/src/main/java/PingDisplay/Pinger.java
+++ app/src/main/java/PingDisplay/Pinger.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-import javax.print.URIException;
 
 public class Pinger {
     private Process process = null;
--- app/src/main/java/PingDisplay/Window.java
+++ app/src/main/java/PingDisplay/Window.java
@@ -4,7 +4,6 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
@@ -13,10 +12,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
@@ -31,6 +27,7 @@ public class Window extends Canvas implements MouseMotionListener, MouseListener
 	public volatile boolean hovering;
 	public volatile boolean mouseInside;
 	public volatile int mouseX, mouseY;
+    public volatile int startX = 0, startY = 0;
 	
 	public double actualCamY;
 	public double targetCamY;
@@ -209,8 +206,8 @@ public class Window extends Canvas implements MouseMotionListener, MouseListener
 	
 	public void mouseDragged(MouseEvent e)
 	{
-		int deltaX = e.getXOnScreen() - mouseX;
-		int deltaY = e.getYOnScreen() - mouseY;
+		int deltaX = e.getX() - startX;
+		int deltaY = e.getY() - startY;
 		int finX = frame.getLocationOnScreen().x + deltaX; //Final x coord of window
 		int finY = frame.getLocationOnScreen().y + deltaY; //Final y coord of window
 		
@@ -255,7 +252,8 @@ public class Window extends Canvas implements MouseMotionListener, MouseListener
 	
 	public void mousePressed(MouseEvent e)
 	{
-		
+		startX = e.getX();
+        startY = e.getY();
 	}
 	
 	public void mouseReleased(MouseEvent e)
